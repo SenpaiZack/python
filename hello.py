b30ccc33faf4ecff937a135de35ca04280a422e8
SenpaiZack <90527849+SenpaiZack@users.noreply.github.com>
2022-01-15 22:31:22 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What is the total bill? $"))
-tip = int(input("How much tip would you like to pay? 10, 12, or 15? "))
-people = int(input("How many people to slipt the bill? "))
-per_person_payment = ("{:.2f}".format(bill * (1 + tip/100)/people))
-print("Each person should pay: $",per_person_payment)
